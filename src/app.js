d8464884e4d0c07009620a97784a2ef5d8e95a11
Gleisson Santos Silva <gtsasil@gmail.com>
2023-02-13 19:06:03 +0000

    End of development

--- src/app.js
+++ src/app.js
@@ -1,11 +1,36 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".card").classList.add(generateRandomSuit());
+  document.querySelector(".number").innerHTML = generateRandomNumber();
+};
+
+let generateRandomNumber = () => {
+  let numberCards = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let indexNumber = Math.floor(Math.random() * numberCards.length);
+
+  return numberCards[indexNumber];
+};
+
+let generateRandomSuit = () => {
+  let suitElements = ["diamond", "heart", "spade", "club"];
+
+  let indexSuit = Math.floor(Math.random() * suitElements.length);
+  return suitElements[indexSuit];
 };
